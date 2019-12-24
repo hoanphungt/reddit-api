f96b6b6ff3cef340f6fb59241c0fcbdc9fa81de1
hoanphungt <hoanphung.cpo@gmail.com>
2019-12-24 16:45:55 +0100

    Add redux dev tool and improve store and actions

--- src/action.js
+++ src/action.js
@@ -7,6 +7,17 @@ export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 
 // ACTIONS
+
+export const selectSubreddit = (subreddit) => ({
+    type: SELECT_SUBREDDIT,
+    subreddit
+});
+
+export const invalidateSubreddit = (subreddit) => ({
+    type: INVALIDATE_SUBREDDIT,
+    subreddit
+});
+
 export const fetchPosts = (subreddit) => (dispatch) => {
     dispatch({
         type: REQUEST_POSTS,
